29f91f31f6345584c65556a803bbde967ddca240
Daniel Serkowski <dserkowski@soldevelo.com>
2018-04-16 15:56:20 +0200

    ITOS-15: Added logging incomming errors

--- api/src/main/java/org/openmrs/module/outgoingmessageexceptions/api/handler/BaseHandlingServiceImpl.java
+++ api/src/main/java/org/openmrs/module/outgoingmessageexceptions/api/handler/BaseHandlingServiceImpl.java
@@ -6,11 +6,15 @@ import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.outgoingmessageexceptions.OutgoingMessage;
 import org.openmrs.module.outgoingmessageexceptions.api.OutgoingMessageExceptionsService;
 import org.openmrs.module.outgoingmessageexceptions.api.model.enums.MessageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 
 public abstract class BaseHandlingServiceImpl extends BaseOpenmrsService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(BaseHandlingServiceImpl.class);
 	
 	private static final int ADMIN_USER_ID = 1;
 	
@@ -19,6 +23,14 @@ public abstract class BaseHandlingServiceImpl extends BaseOpenmrsService {
 	
 	public void handle(String messageBody, String destination,
 			boolean failure, String failureReason) {
+		
+		LOGGER.info("Invoked error handler.%n"
+				+ "Error messageType: {},%n"
+				+ "MessageBody: {},%n"
+				+ "Destination: {},%n"
+				+ "Failure: {},%n"
+				+ "FailureReason {},%n",
+				new Object[]{ getMessageType(), messageBody, destination, failure, failureReason });
 		
 		OutgoingMessage message = new OutgoingMessage();
 		message.setOwner(getOwner());
