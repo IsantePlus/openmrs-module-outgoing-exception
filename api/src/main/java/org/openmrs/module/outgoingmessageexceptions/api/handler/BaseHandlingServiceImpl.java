474912cf6af2d8059fcafbbcf4406a2bf4a24fb8
Daniel Serkowski <dserkowski@soldevelo.com>
2018-04-16 15:44:45 +0200

    ITOS-15: Extracted BaseHandlingsService

--- api/src/main/java/org/openmrs/module/outgoingmessageexceptions/api/handler/BaseHandlingServiceImpl.java
+++ api/src/main/java/org/openmrs/module/outgoingmessageexceptions/api/handler/BaseHandlingServiceImpl.java
@@ -1,37 +1,45 @@
-package org.openmrs.module.outgoingmessageexceptions.api.impl;
+package org.openmrs.module.outgoingmessageexceptions.api.handler;
 
+import org.openmrs.User;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.outgoingmessageexceptions.OutgoingMessage;
 import org.openmrs.module.outgoingmessageexceptions.api.OutgoingMessageExceptionsService;
-import org.openmrs.module.registrationcore.api.MpiErrorHandlingService;
-import org.openmrs.module.xdssender.api.service.XdsErrorHandlingService;
+import org.openmrs.module.outgoingmessageexceptions.api.model.enums.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Component("outgoingmessageexceptions.MpiErrorHandlingService")
-public class MpiErrorHandlingServiceImpl extends BaseOpenmrsService implements MpiErrorHandlingService, XdsErrorHandlingService {
+public abstract class BaseHandlingServiceImpl extends BaseOpenmrsService {
+	
+	private static final int ADMIN_USER_ID = 1;
 	
 	@Autowired
 	private OutgoingMessageExceptionsService outgoingMessageExceptionsService;
 	
-	@Override
-	public void handle(String messageBody, String failureReason, String destination, String type, Boolean failure) {
+	public void handle(String messageBody, String destination,
+			boolean failure, String failureReason) {
 		
 		OutgoingMessage message = new OutgoingMessage();
-		message.setOwner(Context.getUserService().getUser(1));
+		message.setOwner(getOwner());
 		message.setDestination(destination);
 		message.setFailure(failure);
 		message.setFailureReason(failureReason);
 		message.setMessageBody(messageBody);
-		message.setType(type);
+		message.setType(getMessageType().name());
 		message.setTimestamp(new Date());
 		message.setRetried(false);
 		message.setRetryReason("");
 		message.setRetryTimestamp(new Date());
 		
 		outgoingMessageExceptionsService.saveItem(message);
+	}
+	
+	abstract public MessageType getMessageType();
+	
+	private User getOwner() {
+		return Context.getAuthenticatedUser() != null
+				? Context.getAuthenticatedUser()
+				: Context.getUserService().getUser(ADMIN_USER_ID);
 	}
 }
